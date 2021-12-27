eb6b650bfead4c9a5d8d9d299cad159d6a7803e2
sunshineqt <2323123474@qq.com>
2021-12-27 20:04:55 +0800

    自定义渲染器

---
+++ src/CanvasApp.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <piechart @click="handleClick" :data="state.data" :x="200" :y="200" :r="200" />
+  </div>
+</template>
+
+<script>
+import { reactive, ref } from 'vue';
+
+export default {
+  setup () {
+    const state = reactive({
+      data: [
+        {name: 'kk', count: 200, color: 'brown'},
+        {name: 'll', count: 230, color: 'yellow'},
+        {name: 'uu', count: 500, color: 'pink'},
+        {name: 'sa', count: 600, color: 'skyblue'}
+      ]
+    });
+
+    function handleClick() {
+      console.log('click-0');
+      state.data.push({name: 'te', count: 30, color: 'orange'})
+    }
+
+    return {
+      state,
+      handleClick,
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
